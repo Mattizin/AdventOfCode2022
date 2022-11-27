899faee45b92eba3c8ea42cf71c80876583191ca
Martin Schulze Beckendorf <martin.sb@outlook.de>
2022-11-27 18:22:17 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# AdventOfCode2022
+Meine Lösungen für Advent of Code 2022 https://adventofcode.com/2022
+
+Dieses Jahr verwende ich die zur Zeit aktuellste Java Version 19.0.1.
---
+++ src/main/java/de/martinschulzebeckendorf/aoc2022/Main.java
@@ -0,0 +1,7 @@
+package de.martinschulzebeckendorf.aoc2022;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Advent of Code 2022!");
+    }
+}
