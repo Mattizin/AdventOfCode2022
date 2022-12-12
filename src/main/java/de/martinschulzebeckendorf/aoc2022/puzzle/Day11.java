989e3a7e495f522911edc35aea8eb89d1b8e10fc
Martin Schulze Beckendorf <martin.sb@outlook.de>
2022-12-12 09:30:55 +0100

    Day11 Part 2 Fix: Mutable input state

--- src/main/java/de/martinschulzebeckendorf/aoc2022/puzzle/Day11.java
+++ src/main/java/de/martinschulzebeckendorf/aoc2022/puzzle/Day11.java
@@ -7,14 +7,19 @@ import java.util.List;
 
 public class Day11 extends Day {
 
-    private final List<Monkey> monkeys = new ArrayList<>();
+    private final List<String> monkeyInstructions;
 
     public Day11(String inputFileName) {
         super(inputFileName);
 
+        this.monkeyInstructions = this.inputText.lines().map(String::trim).toList();
+    }
+
+    private List<Monkey> parseMonkeys() {
+        final List<Monkey> monkeys = new ArrayList<>();
         // Input parsing
         Monkey monkey = null;
-        for (String line : this.inputText.lines().map(String::trim).toList()) {
+        for (String line : this.monkeyInstructions) {
             //Ignore
             if (line.isBlank() || line.startsWith("Monkey")) {
                 continue;
@@ -41,34 +46,39 @@ public class Day11 extends Day {
             if (line.startsWith("If false")) {
                 monkey.setMonkeyFalseIndex(monkeyIndex);
                 // Full monkey parsed
-                this.monkeys.add(monkey);
+                monkeys.add(monkey);
                 monkey = null;
             }
         }
+        return monkeys;
     }
 
     public long businessLevelMostActiveMonkey(int rounds, boolean worryRelief) {
+        // Local Input parsing or else calling the method multiple times would cause side effects (mutable state)
+        List<Monkey> monkeys = this.parseMonkeys();
+
         long testDivisorProduct = 0L;
+        // Part 2 relevant
         if (!worryRelief) {
             // Math idea by https://todd.ginsberg.com/post/advent-of-code/2022/day11/
             // instead of storing the real worrying level, store the modulo with the leas common divisor of all test "divide by" cases
-            testDivisorProduct = this.monkeys.stream()
+            testDivisorProduct = monkeys.stream()
                     .mapToLong(Monkey::getDivisibleBy)
                     .reduce((a, b) -> a * b)
                     .orElse(0L);
         }
 
         for (int round = 1; round <= rounds; round++) {
-            for (Monkey monkey : this.monkeys) {
+            for (Monkey monkey : monkeys) {
                 for (MonkeyItem item : monkey.getItems()) {
                     MonkeyItem itemInspected = monkey.inspectItem(item, worryRelief, testDivisorProduct);
-                    this.monkeys.get(monkey.getAfterInspectMonkeyReceiverIndex(itemInspected.worryLevel())).addItem(itemInspected);
+                    monkeys.get(monkey.getAfterInspectMonkeyReceiverIndex(itemInspected.worryLevel())).addItem(itemInspected);
                 }
                 monkey.getItems().clear();
             }
         }
 
-        List<Long> sortedInspectCounts = new ArrayList<>(this.monkeys.stream()
+        List<Long> sortedInspectCounts = new ArrayList<>(monkeys.stream()
                 .map(Monkey::getInspectCount)
                 .sorted()
                 .toList());
@@ -99,6 +109,7 @@ class Monkey {
         if (worryRelief) {
             worryLevel /= 3;
         } else {
+            // Part 2 relevant
             // Math idea by https://todd.ginsberg.com/post/advent-of-code/2022/day11/
             worryLevel %= testDivisorProduct;
         }
